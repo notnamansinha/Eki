06026ecbee327b31418a057e15f75e6372a4370f
naman <notnamansinha@gmail.com>
2026-07-16 13:33:14 +0530

    delete: remove speculative simulation routes and features

--- frontend/src/app/extra/layout.tsx
+++
@@ -1,9 +0,0 @@
-import MapProviders from "@/components/MapProviders";
-
-export default function ExtraLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <MapProviders>
-      {children}
-    </MapProviders>
-  );
-}
